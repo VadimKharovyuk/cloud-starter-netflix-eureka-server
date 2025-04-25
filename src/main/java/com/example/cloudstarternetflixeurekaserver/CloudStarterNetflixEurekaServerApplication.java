6ca4c76b1a4702c3f77ef02ea43c9795038c8b50
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-04-26 00:25:49 +0300

    Initial commit

---
+++ src/main/java/com/example/cloudstarternetflixeurekaserver/CloudStarterNetflixEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudstarternetflixeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStarterNetflixEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudStarterNetflixEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloudstarternetflixeurekaserver/CloudStarterNetflixEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudstarternetflixeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStarterNetflixEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
